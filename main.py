788d595cfec927adbd1633b35a395dd79cbd10b4
Shubham Joshi <indsjos@gmail.com>
2026-05-11 09:50:12 +0530

    logger

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from src.CineFlicx.exception.exception_handler import CustomException
+from src.CineFlicx.logger.log import logging
 import sys
 
 
 if __name__ == "__main__":
     try:
+        logging.info(f"logger is working fine")
         a=10/0
+     
+
     except Exception as e:
         raise CustomException(e,sys)
+    
